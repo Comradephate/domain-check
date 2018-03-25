c8db91f58082ed276094f1644c6f816dad9860cb
Jaron Rolfe <me@jaronrolfe.com>
2018-03-24 21:48:09 -0400

    fixed the intentionally failing test

--- domaincheck/router_test.go
+++ domaincheck/router_test.go
@@ -3,6 +3,5 @@ package domaincheck
 import "testing"
 
 func Test_NewRouter(t *testing.T) {
-	t.Fatalf("intentionally failing to see what buildkite does")
-	//t.Skip("not implemented")
+	t.Skip("not implemented")
 }
